f0758e019619670653f70ba0b529b521cef7d591
Inflation <2375962+inflation@users.noreply.github.com>
2024-04-30 16:31:11 +0800

    ci: 👷 Add windows testing

--- vapoursynth4-sys/build.rs
+++ vapoursynth4-sys/build.rs
@@ -12,16 +12,21 @@ fn main() {
         const LIBRARY_DIR_VARIABLE: &str = "VAPOURSYNTH_LIB_PATH";
 
         // Make sure the build script is re-run if our env variable is changed.
-        println!("cargo:rerun-if-env-changed={}", LIBRARY_DIR_VARIABLE);
+        println!("cargo:rerun-if-env-changed={LIBRARY_DIR_VARIABLE}");
 
         if let Ok(dir) = env::var(LIBRARY_DIR_VARIABLE) {
-            println!("cargo:rustc-link-search=native={}", dir);
+            println!("cargo:rustc-link-search=native={dir}");
         }
 
         // Handle linking to VapourSynth libs.
         println!("cargo:rustc-link-lib=vapoursynth");
 
         #[cfg(feature = "vsscript")]
-        println!("cargo:rustc-link-lib=vapoursynth-script");
+        {
+            #[cfg(not(target_os = "windows"))]
+            println!("cargo:rustc-link-lib=vapoursynth-script");
+            #[cfg(target_os = "windows")]
+            println!("cargo:rustc-link-lib=VSScript");
+        }
     }
 }
