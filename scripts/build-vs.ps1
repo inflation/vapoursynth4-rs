001c757cd77f372ec3820658eaf1e3ea1e7c5f5e
Inflation <2375962+inflation@users.noreply.github.com>
2024-12-09 13:31:29 +0800

    ci: Unify Windows build

--- scripts/build-vs.ps1
+++ scripts/build-vs.ps1
@@ -1,4 +1,20 @@
-$env:VSPYTHON_PATH = (Split-Path -Path (Get-Command python.exe).Path)
-msbuild scripts/build-vs.slnf /t:Build /p:Configuration=Debug /p:Platform=x64 /m `
-  /p:UseMultiToolTask=true /p:EnforceProcessCountAcrossBuilds=true `
-  /p:DefineConstants=VS_USE_LATEST_API /p:DefineConstants=VSSCRIPT_USE_LATEST_API
+$VAPOURSYNTH_LIB_PATH = "$PWD/vapoursynth/msvc_project/x64/Debug"
+$DLL_PATH = "$VAPOURSYNTH_LIB_PATH/VapourSynth.dll"
+
+if (!(Test-Path -Path $DLL_PATH)) {
+  pip install Cython
+
+  if (Test-Path -Path vapoursynth) { Remove-Item -Recurse -Force vapoursynth }
+  git clone --depth 1 --branch R70 https://github.com/vapoursynth/vapoursynth
+  git -C vapoursynth clone --depth 1 --branch release-3.0.5 https://github.com/sekrit-twc/zimg
+
+  $env:VSPYTHON_PATH = (Split-Path -Path (Get-Command python.exe).Path)
+  msbuild scripts/build-vs.slnf /t:Build /p:Configuration=Debug /p:Platform=x64 /m `
+    /p:UseMultiToolTask=true /p:EnforceProcessCountAcrossBuilds=true `
+    /p:DefineConstants=VS_USE_LATEST_API /p:DefineConstants=VSSCRIPT_USE_LATEST_API
+}
+
+New-Item -ItemType SymbolicLink -Target $DLL_PATH -Path target/debug/deps/VapourSynth.dll -Force
+Get-ChildItem $VAPOURSYNTH_LIB_PATH/VapourSynth.lib
+
+"VAPOURSYNTH_LIB_PATH=${VAPOURSYNTH_LIB_PATH}" >> $env:GITHUB_ENV
--- scripts/build-vs.sh
+++ scripts/build-vs.sh
@@ -2,32 +2,38 @@
 
 set -euxo pipefail
 
-PREFIX="$PWD/vapoursynth-build"
+export PREFIX="$PWD/vapoursynth/build"
+export VAPOURSYNTH_LIB_PATH="$PREFIX/lib"
 
-if [ ! -d "$PREFIX" ]; then
+if [ ! -f "$VAPOURSYNTH_LIB_PATH/libvapoursynth.so" ]; then
+    rm -rf vapoursynth && git clone --depth 1 --branch R70 https://github.com/vapoursynth/vapoursynth
+
     if [ "$RUNNER_OS" == "macOS" ]; then
         brew install zimg automake libtool
     fi
 
+    pushd vapoursynth
+
+    if [ ! -f "$VAPOURSYNTH_LIB_PATH/libzimg.a" ]; then
+        git clone --depth 1 --branch release-3.0.5 https://github.com/sekrit-twc/zimg
+        pushd zimg
+        ./autogen.sh
+        ./configure --prefix="$PREFIX"
+        make -j"$(nproc)"
+        make install
+        popd
+    fi
+
     pip install Cython
 
-    git clone --depth 1 --branch release-3.0.5 --recursive --shallow-submodules \
-        https://github.com/sekrit-twc/zimg
-    cd zimg
-    ./autogen.sh
-    ./configure --prefix="$PREFIX"
-    make -j"$(nproc)"
-    make install
-
-    cd ../vapoursynth
-    export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+    export PKG_CONFIG_PATH="$VAPOURSYNTH_LIB_PATH/pkgconfig"
     ./autogen.sh
     ./configure CFLAGS="-g -O0 -w" --prefix="$PREFIX"
     make -j"$(nproc)"
     make install
+    popd
 fi
 
-if [ "$RUNNER_OS" == "Linux" ]; then
-    sudo sh -c "echo $PREFIX/lib > /etc/ld.so.conf.d/vapoursynth.conf"
-    sudo ldconfig
-fi
+echo "VAPOURSYNTH_LIB_PATH=${VAPOURSYNTH_LIB_PATH}" >> $GITHUB_ENV
+echo "LD_LIBRARY_PATH=${VAPOURSYNTH_LIB_PATH}" >> $GITHUB_ENV
+echo "DYLD_LIBRARY_PATH=${VAPOURSYNTH_LIB_PATH}" >> $GITHUB_ENV
